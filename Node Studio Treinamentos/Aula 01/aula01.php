292e1dcb766bb0465b6cc87eb949fa13b1887049
AdemirRamos <ademirramos634@gmail.com>
2022-01-12 14:47:57 -0300

    Adição da Aula 01

---
+++ Node Studio Treinamentos/Aula 01/aula01.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Instalando o PHP</title>
+
+</head>
+
+<body>
+    
+    <?php
+
+        echo 'Olá, mundo!';
+
+        //O PHP é uma linguagem interpretada livre.
+        //O PHP é uma tecnologia criada para operar do lado do servidor ("server-side").
+        //Originalmente, o PHP operava em aplicações presentes e/ou atuantes somente no "server-side".
+        //O PHP é uma tecnologia "server-side" capaz de gerar conteúdos dinâmicos na "web".
+
+        //Como o PHP funciona:
+
+        //O cliente faz uma requisição; o servidor interpreta os código PHP, gera e retorna um HTML que é repassado para o cliente.
+
+        //Site oficial do PHP (com documentação oficial e outros conteúdos): https://www.php.net
+
+        //P. S.: Os comentários no PHP funcionam como os comentários no JS.
+
+    ?>
+
+</body>
+
+</html>
