85c290b1814999c2de4e12c4830e4eb52353bcf5
AdemirRamos <ademirramos634@gmail.com>
2022-01-15 10:53:08 -0300

    Adição da Aula 05

---
+++ Node Studio Treinamentos/Aula 05/aula05.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fundamentos Básicos</title>
+
+</head>
+
+<body>
+    
+    <?php
+
+        echo 'Olá, mundo! <br>';
+        print 'Meu nome é Ademir.';
+
+        //"print" é outra maneira de se escrever mensagens (no PHP).
+        /*Caso tenhamos HTML e PHP juntos, é preciso fechar a "tag PHP"; caso tenhamos somente PHP, não é preciso fechá-la.*/
+
+    ?>
+
+</body>
+
+</html>
