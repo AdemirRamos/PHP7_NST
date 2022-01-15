59d7aecd45e4e567a093499c15c073e10a50ace0
AdemirRamos <ademirramos634@gmail.com>
2022-01-15 11:13:05 -0300

    Adição da Aula 06

---
+++ Node Studio Treinamentos/Aula 06/aula06.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+
+</head>
+
+<body>
+    
+    <?php
+
+        //Para se criar variáveis em PHP, se usa o "$". Exemplo:
+
+        $nome = 'Ademir';
+        $idade = 25;
+        $sexo = 'Feminino';
+
+        //No PHP, assim como no Python, o tipo da variável é definido ao se atribuir algo a ela.
+
+        //Escrevendo os dados na tela (com interpolação):
+
+        echo 'Nome: nome; Idade: $idade; Sexo: $sexo.';
+
+        //As regras para criação de variáveis, em PHP, são iguais a outras linguagens de programação - além de serem "case sensitive".
+
+    ?>
+
+</body>
+
+</html>
