3ffb51b21085d40caf95edecbf06c9a0b4f3ddb0
AdemirRamos <ademirramos634@gmail.com>
2022-01-15 22:39:54 -0300

    Atualização da Aula 12

--- Node Studio Treinamentos/Aula 12/aula12.php
+++ Node Studio Treinamentos/Aula 12/aula12.php
@@ -11,9 +11,55 @@
 <body>
     
     <?php
-    
+
+        //É possível atribuir, aos elementos do "array", um índice:
+
+        $carros = array (1 => 'BMW', 2 => 'Lexus', 3 => 'Camaro', 4 => 'Mustang');
+
+        //A impressão de "arrays" funciona de maneira diferente:
+
+        print_r($carros);
+
+        print '<hr>';
+
+        print $carros[1];
+
+        //Inserindo novos itens em um "array":
+
+        $carros[] = 'Audi'; //Dessa forma, o novo item vai automaticamente para a última posição do "array".
+
+        print '<hr>';
+
+        print_r($carros);
+
+        //Também é possível definir em qual posição do "array" o item entrerá - os demais índices do "array" podem ser redefinidos.
         
-    
+        $carros[6] = 'Honda';
+
+        print '<hr>';
+
+        print_r($carros);
+
+        //Outra forma de se criar "arrays":
+
+        $motos = array();
+
+        $motos[] = 'Yamaha';
+        $motos[] = 'Honda';
+        $motos[2] = 'Suzuki';
+        
+        print '<hr>';
+
+        print_r($motos);
+
+        //Outra forma de se criar "arrays":
+
+        $clientes = ['Ademir', 'Rodrigo', 'Felipe', 'Bia'];
+
+        print '<hr>';
+
+        print_r($clientes);
+
     ?>
 
 </body>
