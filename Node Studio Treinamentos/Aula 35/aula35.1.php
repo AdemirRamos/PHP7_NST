f21c056e757bbe86d07450a191c29a650412dd53
AdemirRamos <ademirramos634@gmail.com>
2022-01-20 10:40:42 -0300

    Atualização da Aula 35

--- Node Studio Treinamentos/Aula 35/aula35.1.php
+++ Node Studio Treinamentos/Aula 35/aula35.1.php
@@ -27,46 +27,15 @@
     
     <?php
 
-        //Verificando (abaixo) se o botão enviar foi pressionado pelo usuário:
+        //Nota: eu não sei o porquê, mas o meu código não está funcionando perfeitamente.
 
         if (isset($_POST['form_submit'])):
-            echo '<p class="texto_vermelho">Dados enviados com sucesso!</p><hr>';
-            
-            echo 'Conteúdo de "$_POST": ';
-
-            var_dump($_POST);
-
-            echo '<hr>';
-
-            //Mesmo que alguns ou nenhum campo seja preenchido, a variável "$_POST" cria um "array" para todos os campos.
-            //P. S.: Os campos não preenchidos, no "array", aparecerão sem nenhum conteúdo, somente com o índice.
-
-            $idade = $_POST['idade'];
-
-            echo "Idade: '$idade'.";
-
-            echo '<hr>';
-
-            //Da maneira como está acima, a idade digitada pelo usuário no campo "Idade" será escrita logo após a descrição do "array".
-
-            //Entendendo a validação de dados: a validação serve para impedir ataques de usuários mal-intencianados
+            //echo '<p class="texto_vermelho">Dados enviados com sucesso!</p><hr>';
 
             $erros = array();
 
-            //Usando "filter_input": 1ª parâmetro: o tipo de "input"; 2ª parâmetro: nome do campo; 3ª: o filtro a ser utilizado: 
-
-            /*if ($idade == filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
-                echo 'A idade digitada é válida.';
-                echo '<hr>';
-
-            else:
-                echo 'A idade digitada não é válida.';
-                echo '<hr>';
-
-            endif;*/
-
             if (!$idade = filter_input(INPUT_POST, 'idade', FILTER_VALIDATE_INT)):
-                $erros[] = 'A idade deve ser um número (inteiro).';
+                $erros[] = 'A idade deve ser um número inteiro.';
 
             endif;
 
@@ -90,9 +59,7 @@
 
             endif;
 
-            //Criando uma condição para a exibição de erros:
-
-            if (!empty($erros)): //Tradução: se a variável "$erros" estiver diferente de vazia.
+            if (!empty($erros)):
                 foreach ($erros as $erro);
 
                     echo "<li>$erro</li>";
@@ -113,15 +80,11 @@
 
     <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post">
 
-    <!--Usando a mesma página do formulário para abrigar o arquivo PHP para onde irão os dados - seguir o exemplo acima ("action").-->
-
     Peso: <input type="text" name="peso" style="margin-bottom: 10px;">
     E-mail: <input type="text" name="email" style="margin-bottom: 10px;">
     Idade: <input type="text" name="idade" style="margin-bottom: 10px;">
     IP: <input type="text" name="ip" style="margin-bottom: 10px;">
     URL: <input type="text" name="url" style="margin-bottom: 10px;">
-
-    <!--Para verificar se o formulário foi preenchido e o usuário clicou em "Enviar", primeiro precisamos dar nome ao botão "Enviar":-->
 
     <button type="submit" name="form_submit">Enviar</button>
 
