718c3154698327c49c5817445213f71021640091
AdemirRamos <ademirramos634@gmail.com>
2022-01-24 11:49:57 -0300

    Adição da Aula 40

---
+++ Node Studio Treinamentos/Aula 40/aula40.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de Login</title>
+
+</head>
+
+<body>
+    
+    <?php
+    
+        /*PHP Procedural: paradigma de programação que indica procedimentos a serem executados pelo "software" ou sistema para se atingir determinado objetivo. Normalmente, esse paradigma é usado a nível pedagógico durante o primeiro contato com programação ou sistemas mais simples.*/
+
+    ?>
+
+</body>
+
+</html>
