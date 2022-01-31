d00d473616425c8b00c6caacf6b16d831230be00
AdemirRamos <ademirramos634@gmail.com>
2022-01-31 13:05:56 -0300

    Atualização da Aula 55

--- Node Studio Treinamentos/Aula 55/aula55.php
+++ Node Studio Treinamentos/Aula 55/aula55.php
@@ -14,8 +14,6 @@
 
         //Expressões Regulares: definem o padrão para ser usado para procurar e/ou substituir palavras.
 
-        $string = 'abc';
-
         //O padrão a ser definido deve ir entre duas barras: "//".
 
         //Operadores:
@@ -34,9 +32,68 @@
 
         //P. S.: Caso a "padrão" tenha mais de uma letra, o retorno será false.
 
-        //P. S.: "i" minúsculo, após a barra, anula o "case sensitive".
+        //"i" minúsculo, após a segunda barra, anula o "case sensitive" que, por definição, é padrão.
 
-        $padrão = '/[a-z]/';
+        //"{}": dentro das chaves se pode colocar o número de caracteres permitidos. Exemplo: "/[a-z]{1-5}/".
+
+        //"?": o retorno só será "true" em caso de uma ou nenhuma ocorrência (caractere).
+
+        //"*": o retorno será "true" independente do número de ocorrências (caracteres).
+
+        //"+": o retorno será "true" somente quando houver um ou mais retorno (s) [ocorrências].
+
+        $string = 'abc123';
+
+        $padrão = "/^[a-z0-9]+$/i";
+
+        if (preg_match($padrão, $string)):
+
+            echo 'Válido. <hr>';
+
+            echo "Resultado: \"$string\". <hr>";
+            
+        else:
+
+            echo 'Inválido. <hr>';
+
+        endif;
+
+    ?>
+
+    <?php
+
+        //1ª exemplo real:
+
+        $string = 'emailnotreal123@gmail.com';
+
+        $padrão = "/^[a-z0-9.\-\_]+@[a-z0-9.\-\_]+\.(com|br|com.br|net)+$/i";
+        
+        //Dentro dos colchetes, estão todos os caracteres aceitos.
+        //Após o sinal de mais, temos declaramos uma @ para que essa seja aceita e validada.
+        //Após o arroba, temos a validação do "gmail".
+        //Após o segundo sinal de mais, adicionamos um ponto para validar o ponto entre "gmail" e "com".
+        //P. S.: Antes de ponto, devemos adicionar uma barra invertida.
+        //Após o ponto, entre parênteses, vão as terminações aceitas - separadas por barra.
+
+        if (preg_match($padrão, $string)):
+
+            echo 'Válido. <hr>';
+
+            echo "Resultado: \"$string\". <hr>";
+            
+        else:
+
+            echo 'Inválido. <hr>';
+
+        endif;
+
+        //2ª exemplo real:
+
+        $string = '13/09/2018';
+
+        $padrão = "/^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/";
+
+        //Assim como o ponto, a barra também precisa de, antes de si, ter uma barra invertida.
 
         if (preg_match($padrão, $string)):
 
